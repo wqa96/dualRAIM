3bc24ff8fda982fd793398562ab20fd899b620d1
wqa96 <37138912+wqa96@users.noreply.github.com>
2024-01-22 21:55:40 +0800

    Add files via upload

--- code/SSETestAnalyze.m
+++ code/SSETestAnalyze.m
@@ -2,44 +2,44 @@
 clear;
 % close all;
 
-addpath data;
-c = 299792458;              % 光速（m/s）
+addpath ..\data;
+c = 299792458;              % 光速spped of light（m/s）
 %% 参数设置
 N = 1000;
 alarmRateInd = 2;
 prangeBias = 100;
 %% 获取可见卫星数据
-navdata = BDSeph('hour0010.20b', '3.03');   %读取某一天的北斗星历文件rinex3.03
-PRN = unique(navdata.prn, 'rows');          %获取所有的卫星编号CXX
+navdata = BDSeph('hour0010.20b', '3.03');   %读取某一天的北斗星历文件rinex3.03 read an rinex3.03 file
+PRN = unique(navdata.prn, 'rows');          %获取所有的卫星编号CXX get the prn number
 PRNNUM = str2num(PRN(:, 2:3));              %PRN编号转为数字XX
 
-refpLLA = [108.930546733333, 34.198421205, 1413.8541];   %参考点经纬高
-[refpX, refpY, refpZ] = LLA2ECEF0(refpLLA(1), refpLLA(2), refpLLA(3));   %参考点ECEF
+refpLLA = [108.930546733333, 34.198421205, 1413.8541];   %参考点经纬高 the reference LLA position(long, lat, alt)
+[refpX, refpY, refpZ] = LLA2ECEF0(refpLLA(1), refpLLA(2), refpLLA(3));   %参考点ECEF the reference ECEF position(X, Y, Z)
 
-t = [2020 1 1 1 1 0];                       %仿真时刻
+t = [2020 1 1 1 1 0];                       %仿真时刻 reference time
 
-satdata = BDSSatPosition(navdata, t, 'ECEF');   %仿真时刻的卫星数据
+satdata = BDSSatPosition(navdata, t, 'ECEF');   %仿真时刻的卫星数据 the satellites' data at reference time
 sat2ref = [satdata.x - refpX, ...
     satdata.y - refpY, ...
     satdata.z - refpZ];                     %卫星位置关于参考位置的矢量
-r = sqrt(sum(sat2ref.^2, 2));               %矢量长度
-satind = ((sat2ref * [refpX; refpY; refpZ]) > 0) & (satdata.sathl == 0);%参考点处可见卫星
+r = sqrt(sum(sat2ref.^2, 2));               %矢量长度vector length
+satind = ((sat2ref * [refpX; refpY; refpZ]) > 0) & (satdata.sathl == 0);%参考点处可见卫星satellites in view
 satind([14, 15, 19, 23, 30, 32, 34, 35]) = false;
-prnnum = PRNNUM(satind);                    %参考点处可见卫星编号
+prnnum = PRNNUM(satind);                    %参考点处可见卫星编号satellites' prn in view
 satlabels = mat2cell(satdata.prn(satind, :), ones(sum(satind), 1), 3);
 %% 可见卫星星座图
 [satE, satN, satU] = ECEF2ENU1(sat2ref(satind, 1), sat2ref(satind, 2), sat2ref(satind, 3), ...
-    refpLLA(1), refpLLA(2));                %可见卫星的ENU坐标
+    refpLLA(1), refpLLA(2));                %可见卫星的ENU坐标satellites' ENU position (E, N, U)
 sat0 = [satE, satN, satU];
-phi = asin(satU ./ r(satind)) * 180 / pi;   %可见卫星的仰角
-theta = atan2(satE, satN) * 180 / pi;       %可见卫星的方位角
-r = sqrt(satE.^2 + satN.^2 + satU.^2);      %可见卫星的真实距离
+phi = asin(satU ./ r(satind)) * 180 / pi;   %可见卫星的仰角satellites' elevation
+theta = atan2(satE, satN) * 180 / pi;       %可见卫星的方位角satellites' Azimuth
+r = sqrt(satE.^2 + satN.^2 + satU.^2);      %可见卫星的真实距离satellites' geometric distance
 % figure(11);
 % skyPlot0(theta, phi, prnnum);
 %% 伪距/检测统计量
-%测量误差
+%测量误差variance of pseudorange measurement noise
 sigmaURE = 4;
-%检测统计量阈值
+%检测统计量阈值the SSE threshold of diffetent alarm rate
 alarmRate = [0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001];
 SSE = zeros(length(alarmRate), 17);
 alpha = 0:0.01:60;
@@ -52,7 +52,7 @@ end
 %检测统计量门限判定，1超过门限
 SPAlarm = @(x, n, m) x'*x > SSE(m, n);
 %定位结果
-pos_r = zeros(1, 3);
+pos_r = zeros(1, 3);  %real position
 pos_s0 = zeros(1, 3);
 %卫星坐标
 satdata = BDSSatPosition(navdata, t, 'ECEF');   %仿真时刻的卫星数据
@@ -60,29 +60,29 @@ sat2ref = [satdata.x(satind) - refpX, ...
     satdata.y(satind) - refpY, ...
     satdata.z(satind) - refpZ];                     %卫星位置关于参考位置的矢量
 [satE, satN, satU] = ECEF2ENU1(sat2ref(:, 1), sat2ref(:, 2), sat2ref(:, 3), ...
-    refpLLA(1), refpLLA(2));        %可见卫星的ENU坐标
+    refpLLA(1), refpLLA(2));        %可见卫星的ENU坐标satellites' ENU position
 sat = [satE, satN, satU];
 %真实伪距
-r = sqrt(sum((sat - pos_r).^2, 2));
+r = sqrt(sum((sat - pos_r).^2, 2)); %satellites' geometric distance
 %% Monte Carlo 仿真
-%所有大于等于5颗星的组合
+%所有大于等于5颗星的组合all combinations of satellites (the satellite number no less than 5)
 satnum = 12;
 satInd = logical(dec2bin(1:2^satnum-1)-'0');
-satnum = sum(satInd, 2);
+satnum = sum(satInd, 2);        %satellite number list
 satInd = satInd(satnum > 5, :);
 satnum = satnum(satnum > 5);
 Num = length(satnum);
 % 观测量
-missDet1 = nan(Num, 12);        %单异常SSE检测漏检
+missDet1 = nan(Num, 12);        %单异常SSE检测漏检miss detection rate of one fault
 missDet1all = nan(Num, 12, N);
-missDet2 = nan(Num, 66);        %双异常SSE检测漏检
+missDet2 = nan(Num, 66);        %双异常SSE检测漏检miss detection rate of two fault
 missDet2all = nan(Num, 66, N);
-Sii = nan(Num, 12);             %单异常放缩因子
+Sii = nan(Num, 12);             %单异常放缩因子the GIF of one fault
 Fi = nan(Num, 12, N);
 Pij = nan(Num, 66, 2);          %双异常
-Sij = nan(Num, 66, 3);
+Sij = nan(Num, 66, 3);          %the matrix Qij in Eq. 35
 Fij = nan(Num, 66, N);
-% 进度条
+% 进度条progress bar
 tElapsed = 0;
 simTotNum = Num;
 simCurNum = 0;
@@ -91,7 +91,7 @@ waitbarWin = waitbar(0, '初始化');
 tic;
 for num = 1:Num
     nn = 1;
-    % 进度条
+    % 进度条progress bar
     simCurNum = num-1;
     simRemNum = simTotNum - simCurNum;
     simSpeed = simCurNum/tElapsed;
@@ -103,35 +103,35 @@ for num = 1:Num
         ['仿真进度: ', num2str(simCurNum/simTotNum*100, '%.2f'), ' % (', ...
         num2str(simCurNum), '/', num2str(simTotNum), ')', ...
         '剩余时间:', num2str(remHour), ':', num2str(remMin, '%02d'), ':', num2str(remSec, '%02d')]);
-    % 当前仿真的卫星参数
+    % 当前仿真的卫星参数current satellites number
     r1 = r(satInd(num, :));
     sat1 = sat(satInd(num, :), :);
-    %接收机时钟误差
+    %接收机时钟误差time bias
     dtr = randn()*1e-7;
     pr = r1 + c*dtr;
-    %% 遍历所有单异常卫星情况
+    %% 遍历所有单异常卫星情况traverse all combinations of one fault
     for kk = 1:satnum(num)
-        % 正常卫星的GDOP
+        % the kkth satellite is fault
         flag = true(satnum(num), 1); flag(kk) = false;
-        sattmp = sat1(flag, :);
-        rtmp = r1(flag);
+        sattmp = sat1(flag, :); %current real satellites' position
+        rtmp = r1(flag);        %current real satellites' distance
         G = -(sattmp - pos_r)./rtmp;
         G(:, 4) = 1;
         H = inv(G'*G);
-        g1 = [-(sat1(~flag, :) - pos_r)./r1(~flag), 1]';
+        g1 = [-(sat1(~flag, :) - pos_r)./r1(~flag), 1]';%the line-of-sight of the fault satellite
         Sii(num, kk) = 1/(1 + g1'*H*g1);
-        % 添加偏移量
+        % 添加偏移量add error bias
         pr1 = pr;
         pr1(kk) = pr1(kk) + prangeBias;
         misscnt = 0;
         %% N次仿真
         for nn = 1:N
-            % 添加噪声
+            % 添加噪声add measurement noise
             URE = sigmaURE*randn(satnum(num), 1);
             pr1nn = pr1 + URE;
-            % 定位解算
+            % 定位解算pvt
             ptmp = [pos_r, 0];
-            [pos, rtmp, perr, G, H, cnt] = positioning(pr1nn, sat1, ptmp, 1e-10, 10);
+            [pos, rtmp, perr, G, H, cnt] = pvtpos0(pr1nn, sat1, ptmp, 1e-10, 10);
             sseFlag = perr'*perr < SSE(alarmRateInd, satnum(num)-4);
             if sseFlag
                 misscnt = misscnt + 1;
@@ -143,37 +143,36 @@ for num = 1:Num
         end
         missDet1(num, kk) = misscnt/N;
     end
-    %% 遍历所有双异常卫星情况
+    %% 遍历所有双异常卫星情况traverse all combinations of two faults
     if satnum(num) < 7
-        continue;
+        continue;   %satellite number must be more than 7
     end
     flags = nchoosek(1:satnum(num), 2);
     for kk = 1:size(flags, 1)
-        % 正常卫星的GDOP
         flag = true(satnum(num), 1); flag(flags(kk, :)) = false;
         sattmp = sat1(flag, :);
         rtmp = r1(flag);
         G = -(sattmp - pos_r)./rtmp;
         G(:, 4) = 1;
         H = inv(G'*G);
-        g1 = [-(sat1(~flag, :) - pos_r)./r1(~flag), [1; 1]]';
-        P = eye(2) + g1'*H*g1;
+        g1 = [-(sat1(~flag, :) - pos_r)./r1(~flag), [1; 1]]';%the line-of-sight of the fault satellites
+        P = eye(2) + g1'*H*g1;%the matrix Qij in Eq. 35
         [V, D] = eig(P);
         Pij(num, kk, :) = diag(D);
         Sij(num, kk, 1:2) = diag(P);
         Sij(num, kk, 3) = P(1, 2);
-        % 添加偏移量
+        % 添加偏移量add error bias
         pr1 = pr;
         pr1(~flag) = pr1(~flag) + prangeBias;
         misscnt = 0;
         %% N次仿真
         for nn = 1:N
-            % 添加噪声
+            % 添加噪声add measurement noise
             URE = sigmaURE*randn(satnum(num), 1);
             pr1nn = pr1 + URE;
-            % 定位解算
+            % 定位解算pvt
             ptmp = [pos_r, 0];
-            [pos, rtmp, perr, G, H, cnt] = positioning(pr1nn, sat1, ptmp, 1e-10, 10);
+            [pos, rtmp, perr, G, H, cnt] = pvtpos0(pr1nn, sat1, ptmp, 1e-10, 10);
             sseFlag = perr'*perr < SSE(alarmRateInd, satnum(num)-4);
             if sseFlag
                 misscnt = misscnt + 1;
@@ -184,13 +183,13 @@ for num = 1:Num
     tElapsed = toc;
 end
 close(waitbarWin);
-%% 数据保存
+%% 数据保存save data
 t = datetime('now');
 tMDHM = reshape(num2str([t.Month, t.Day, t.Hour, t.Minute]', '%02d')', 1, []);
 fullpath = mfilename('fullpath');
 [~, name] = fileparts(fullpath);
 simMode = num2str(prangeBias');
-savefilename = ['.\data\', name, '_', tMDHM, '_', simMode, '_', num2str(alarmRateInd), '.mat'];
+savefilename = ['..\data\', name, '_', tMDHM, '_', simMode, '_', num2str(alarmRateInd), '.mat'];
 save(savefilename);
 %% 单异常卫星分析
 satNumMin = 6;
@@ -370,7 +369,7 @@ for nn = 6:6
     ylabel('Miss detection rate');
     legend('simulation result', 'ideal');
 end
-%%
+%% find the minimum GIF and the corresponding satellite constellation
 flag = satnum == 6;
 xtmp = Sii(flag, :);
 ytmp = missDet1(flag, :);
@@ -425,7 +424,7 @@ for nn = 7:7
     ylabel('Miss detection rate');
     legend('simulation result', 'ideal');
 end
-%%
+%% find the minimum rho_ij and the corresponding satellite constellation
 flag = satnum == 7;
 xtmp = (Sij(flag, :, 1) + Sij(flag, :, 2) - 2*Sij(flag, :, 3))./(Sij(flag, :, 1).*Sij(flag, :, 2)-Sij(flag, :, 3).^2);
 ytmp = missDet2(flag, :);
